f86122f9faa10a0724e85c444bbc5b0dab605129
gcaino <german.caino@hotmail.com>
2016-04-21 19:34:40 -0300

    Lógica en Cliente con idUsuario

--- php/controllerCliente.php
+++ php/controllerCliente.php
@@ -25,31 +25,40 @@ switch ($metodo) {
  
         break;
     case 'post':
-        
-        $cli = json_decode($_POST['cliente']);
-        $domicilio = new Domicilio($cli->Calle, $cli->Numero, $cli->Piso, $cli->Depto, $cli->Localidad, $cli->Provincia, 
-                         $cli->Pais, $cli->CP);
-        // Paso el objeto domicilio a cliente
-        $cliente = new Cliente(null, $cli->Dni, $cli->Cuil,$cli->Apellido, $cli->Nombre, $cli->Telefono, $cli->Email, $domicilio);
+        // Almaceno idUsuario
+        $idUsuario = $_SESSION['idUsuario'];
+        $cli = Cliente::getByIdUsuario($idUsuario);
+        // Valido si el usuario tiene un perfil de cliente creado o inactivo 'baja = true'
+        if($cli == null || $cli->baja == true) {
+            $cli = json_decode($_POST['cliente']);
+            $domicilio = new Domicilio($cli->Calle, $cli->Numero, $cli->Piso, $cli->Depto, $cli->Localidad, $cli->Provincia, 
+                             $cli->Pais, $cli->CP);
+            // Paso el objeto domicilio a cliente
+            $cliente = new Cliente(null, $cli->Dni, $cli->Cuil,$cli->Apellido, $cli->Nombre, $cli->Telefono, $cli->Email, 
+                                   $domicilio, $idUsuario, null);
 
-        /* VALIDACIONES */
-        $validator = new Validator;
-        // Validación datos cliente
-        $validator->validate($cliente, Cliente::$reglas);
-        if(!$validator->tuvoExito()){
-          echo json_encode($validator->getErrores());
-          exit;
-        }
-        // Validación datos domicilio
-        $validator->validate($domicilio, Domicilio::$reglas);
-        if(!$validator->tuvoExito()){
-          echo json_encode($validator->getErrores());
-          exit;
-        }
-        // Validación INSERT en tablas clientes y domicilios
-        if(Cliente::insertCliente($cliente)) {
-          echo json_encode("exito");
-          exit;
+            /* VALIDACIONES */
+            $validator = new Validator;
+            // Validación datos cliente
+            $validator->validate($cliente, Cliente::$reglas);
+            if(!$validator->tuvoExito()){
+              echo json_encode($validator->getErrores());
+              exit;
+            }
+            // Validación datos domicilio
+            $validator->validate($domicilio, Domicilio::$reglas);
+            if(!$validator->tuvoExito()){
+              echo json_encode($validator->getErrores());
+              exit;
+            }
+            // Validación INSERT en tablas clientes y domicilios
+            if(Cliente::insertCliente($cliente)) {
+                echo json_encode("exito");
+                exit;
+            }
+        } else {
+            echo json_encode("El usuario ya tiene un perfil de cliente activo.");
+            exit;
         }
            
         break;
@@ -59,7 +68,7 @@ switch ($metodo) {
         parse_str(file_get_contents("php://input"),$delete_vars);
         $idCliente = $delete_vars['idCliente'];
         
-        if (!isset($id)) die("Error: no hay un id");
+        if (!isset($idCliente)) die("Error: no hay un id");
 
         if(Cliente::eliminar($idCliente)) {
             echo json_encode("exito");
