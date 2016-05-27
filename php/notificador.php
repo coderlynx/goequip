8fa1e40c58f12d1331f6e64758c27416e19b343b
ferca84 <fernandocaino84@gmail.com>
2016-05-27 09:59:44 -0300

    cambio en codigoComun.html y notificador.php

--- php/notificador.php
+++ php/notificador.php
@@ -18,7 +18,7 @@ try
 	// Ahora vamos a guardar que este cliente pago el servicio $servicio.
 
 	## Consulto la base de datos a ver si tengo guardado el ID del pago.
-    /*$query = "SELECT count(*) as cantidad from pagos where collection_id = :collection_id";
+    $query = "SELECT count(*) as cantidad from pagos where collection_id = :collection_id";
 
     $stmt = DBConnection::getStatement($query);
     
@@ -38,30 +38,68 @@ try
             $stmt = DBConnection::getStatement($query);
     
             $fecha = date('Y/m/d H:i:s');
-            $stmt->bindParam(':collection_id', $data,PDO::PARAM_INT);
+            $stmt->bindParam(':collection_id', $data['id'],PDO::PARAM_INT);
             $stmt->bindParam(':estado', $status,PDO::PARAM_STR);
-            $stmt->bindParam(':idPedido', $data,PDO::PARAM_INT);
+            $stmt->bindParam(':idPedido', $data['id'],PDO::PARAM_INT);
             $stmt->bindParam(':fecha', $fecha,PDO::PARAM_STR);
+           
+           if(!$stmt->execute()) {
+                mail("fernandocaino84@gmail.com","Error al guardar el pago","No se pudo insertar un nuevo pago.");
+                //throw new Exception("Error en buscar el pago.");
+            }
+           
+           
+           $mensaje_cliente = "Estimado " + data['payer']['first_name'] + ' ' + data['payer']['last_name'] + ',<br/><br/> 
+            Este es su recibo de pago de la factura ' + data['id'] + 'enviada el ' + $fecha + ' de su compra en GOEQUIP por un total de $ ' + data['payer']['transaction_amount'] + '.<br />Estado: ' + data['status'] + '<br /><br />Este correo servirá como comprobante oficial de este pago.';
+
+            $mensaje_duenio = "Has concretado una venta.<br /><br />El cliente " + data['payer']['first_name'] + ' ' + data['payer']['last_name'] + ', ha realizado el pago por un total de $ ' + data['payer']['transaction_amount'] + '.<br />El estado del pago es: ' + data['status'] + '.';
+
+            $msg = print_r($data,1);
+            // send email a cliente
+            $mail_cliente = $data['payer']['email'];
+            mail($mail_duenio,"Confirmación de Pago GOEQUIP",$msg);
+            //send mail a dueño
+            $mail_duenio = $data['collector']['email'];
+            mail($mail_duenio,"Notificación de Venta realizada",$msg);
        } else {
            ## ESTE ES IMPORTANTE
             ## SI EL ID DEL PAGO YA LO TENIAMOS REGISTRADO, ENTONCES ES QUE ####EXISTIO#### UN CAMBIO DE ESTADO ( $data["status"] y $data["status_detail"] lo explican ) 
             ## HACER EL PROCESO QUE SEA NECESARIO.
 
-            /*EJ: SI un usuario nos paga por pagofacil, cuando extrae el cupon nos llega una notificacion y cuando se acredita el dinero de pagofacil nos llega otra.
-            $query = "INSERT INTO pagos (collection_id, estado, idPedido, fecha)
-				VALUES(:collection_id, :estado, :idPedido, :fecha)";
+            /*EJ: SI un usuario nos paga por pagofacil, cuando extrae el cupon nos llega una notificacion y cuando se acredita el dinero de pagofacil nos llega otra.*/
+            /*$query = "INSERT INTO pagos (collection_id, estado, idPedido, fecha)
+				VALUES(:collection_id, :estado, :idPedido, :fecha)";*/
+           $query = "UPDATE pagos set estado = :estado AND fecha = :fecha";
            
             $stmt = DBConnection::getStatement($query);
     
             $fecha = date('Y/m/d H:i:s');
-            $stmt->bindParam(':collection_id', $data,PDO::PARAM_INT);
+            //$stmt->bindParam(':collection_id', $data,PDO::PARAM_INT);
             $stmt->bindParam(':estado', $status,PDO::PARAM_STR);
-            $stmt->bindParam(':idPedido', $data,PDO::PARAM_INT);
+           // $stmt->bindParam(':idPedido', $data,PDO::PARAM_INT);
             $stmt->bindParam(':fecha', $fecha,PDO::PARAM_STR);
            
-       }*/
+            if(!$stmt->execute()) {
+                mail("fernandocaino84@gmail.com","Error al actualizar el pago","No se pudo actualizar el pago " + data['id']);
+                //throw new Exception("Error en buscar el pago.");
+            }
+           
+          $mensaje_cliente = "Estimado " + data['payer']['first_name'] + ' ' + data['payer']['last_name'] + ',<br/><br/> 
+            Este es su recibo de pago de la factura ' + data['id'] + 'enviada el ' + $fecha + ' de su compra en GOEQUIP por un total de $ ' + data['payer']['transaction_amount'] + '.<br />Estado: ' + data['status'] + '<br /><br />Este correo servirá como comprobante oficial de este pago.';
 
-   // }
+            $mensaje_duenio = "Has concretado una venta pendiente.<br /><br />El cliente " + data['payer']['first_name'] + ' ' + data['payer']['last_name'] + ', ha realizado el pago por un total de $ ' + data['payer']['transaction_amount'] + ' del pedido número ' + data['id'] + ' que se encontraba pendiente.<br />El estado del pago es: ' + data['status'] + '.';
+
+            $msg = print_r($data,1);
+            // send email a cliente
+            $mail_cliente = $data['payer']['email'];
+            mail($mail_duenio,"Confirmación de Pago GOEQUIP",$msg);
+            //send mail a dueño
+            $mail_duenio = $data['collector']['email'];
+            mail($mail_duenio,"Notificación de Venta realizada",$msg);
+           
+       }
+
+    }
     
 	//$sql = "select count(*) as cantidad from pagos where collection_id = '".$data["id"]."' ";
 	//$rs = $conn->execute($sql);
@@ -85,9 +123,19 @@ try
 
 	## NOTIFICAMOS VIA EMAIL QUE TODO ESTA BIEN Y QUE SALIO TODO COMO QUERIAMOS ( Y el dinero esta en nuestras cuentas ya )
     // the message
+      
+    /*$mensaje_cliente = "Estimado " + data['payer']['first_name'] + ' ' + data['payer']['last_name'] + ',<br/><br/> 
+    Este es su recibo de pago de la factura ' + data['id'] + 'enviada el ' + $fecha + ' de su compra en GOEQUIP por un total de $ ' + data['payer']['transaction_amount'] + '.<br />Estado: ' + data['status'] + '<br /><br />Este correo servirá como comprobante oficial de este pago.';
+        
+    $mensaje_duenio = "Has concretado una venta.<br /><br />El cliente " + data['payer']['first_name'] + ' ' + data['payer']['last_name'] + ', ha realizado el pago por un total de $ ' + data['payer']['transaction_amount'] + '.<br />El estado del pago es: ' + data['status'] + '.';
+    
     $msg = print_r($data,1);
-    // send email
-    mail("fernandocaino84@gmail.com","My subject",$msg);
+    // send email a cliente
+    $mail_cliente = $data['payer']['email'];
+    mail($mail_duenio,"Confirmación de Pago GOEQUIP",$msg);
+    //send mail a dueño
+    $mail_duenio = $data['collector']['email'];
+    mail($mail_duenio,"Notificación de Venta realizada",$msg);*/
 
 	/*$sendmail = new PHPMailer();
 	$sendmail->IsHTML(false);
@@ -124,7 +172,8 @@ try
     
     $msg = "GET:\n".print_r($_GET,1)."\n".$e->getMessage();
     // send email
-    mail("fernandocaino84@gmail.com","My subject",$msg);
+    $mail_duenio = $data['collector']['email'];
+    mail($mail_duenio,"Error de conectividad",$msg);
     
 	/*$sendmail = new PHPMailer();
 	$sendmail->IsHTML(false);
@@ -155,7 +204,8 @@ header($_SERVER['SERVER_PROTOCOL'] . ' 500 Internal Server Error', true, 500);
 
 $msg = "GET:\n".print_r($_GET,1)."\n"."SERVER:\n".print_r($_SERVER,1);
 // send email
-mail("fernandocaino84@gmail.com","My subject",$msg);
+$mail_duenio = $data['collector']['email'];
+mail($mail_duenio,"Error: hubo un movimiento en la cuenta pero no se reconocio",$msg);
 
 /*$sendmail = new PHPMailer();
 $sendmail->IsHTML(false);
